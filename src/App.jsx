07355761c9ecc6d5be1a1020e5d3dfd3be4ead4e
Clinton <clintonlim15@gmail.com>
2023-12-13 02:03:13 +1100

    Learning map functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import * as React from 'react';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://reactjs.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
+function getTitle (book) {
+  return book.title + " ";
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>{list.map(getTitle)}</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
     </div>
   )
 }
